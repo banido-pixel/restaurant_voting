afdc7caed269592760c71bcb4900459575acb334
Grigorii Utiugov <wertyas1997@gmail.com>
2022-01-16 09:14:48 +0300

    Move NotFoundException

--- src/main/java/ru/javaops/topjava2/error/NotFoundException.java
+++ src/main/java/ru/javaops/topjava2/error/NotFoundException.java
@@ -1,4 +1,4 @@
-package ru.javaops.topjava2.util.exception;
+package ru.javaops.topjava2.error;
 
 public class NotFoundException extends RuntimeException{
     public NotFoundException(String message) {
