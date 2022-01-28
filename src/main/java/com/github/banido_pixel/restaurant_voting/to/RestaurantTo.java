46a28589cb10a9cfb8c740fefaf6db0f95bd0fb9
Grigorii Utiugov <wertyas1997@gmail.com>
2022-01-28 17:08:43 +0300

    Remove unnecessary constructor and interface implementation

--- src/main/java/com/github/banido_pixel/restaurant_voting/to/RestaurantTo.java
+++ src/main/java/com/github/banido_pixel/restaurant_voting/to/RestaurantTo.java
@@ -1,6 +1,5 @@
 package com.github.banido_pixel.restaurant_voting.to;
 
-import com.github.banido_pixel.restaurant_voting.HasId;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
 import lombok.Value;
@@ -8,16 +7,12 @@ import lombok.Value;
 @Value
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
-public class RestaurantTo extends NamedTo implements HasId {
+public class RestaurantTo extends NamedTo {
 
-    Integer votesAmount;
-
-    public RestaurantTo(Integer id, String name, Integer votesAmount) {
-        super(id, name);
-        this.votesAmount = votesAmount;
-    }
+    Long votesAmount;
 
     public RestaurantTo(Integer id, String name, Long votesAmount) {
-        this(id, name, votesAmount.intValue());
+        super(id, name);
+        this.votesAmount = votesAmount;
     }
 }
